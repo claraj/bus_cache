cea3516293f35e3e619d50cc8d8aefed32580f27
clara <10088152+claraj@users.noreply.github.com>
2020-10-12 21:43:39 -0500

    space

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from datetime import datetime
 while True:
 
     input('press enter to get bus time ')
-    print('The current date/time is ', datetime.now())
-    
+    print('The current date/time is', datetime.now())
+
     locations = get_bus_locations.get_bus_locations('17940')
 
     print('The next bus will depart:', locations.next_bus_depart())
